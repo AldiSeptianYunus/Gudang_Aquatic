5e72e950f87f83a6b3ba6fc8a8a0a5b716409ee0
AldiSeptianYunus <aldiseptian31@gmail.com>
2025-04-26 17:11:12 +0700

    Add files via upload

---
+++ Apps_Gudang/src/Koneksi/Koneksi.java
@@ -0,0 +1,27 @@
+package koneksi;
+import java.sql.*;
+
+public class Koneksi {
+  private Connection koneksi;
+    
+  public Connection connect() {
+    try {
+      Class.forName("com.mysql.cj.jdbc.Driver");
+      System.out.println("Berhasil load driver");
+    } 
+    catch (ClassNotFoundException ex) {
+      System.out.println("Gagal load driver: " + ex);
+    }
+    
+    String url = "jdbc:mysql://localhost/gudang?useSSL=false&serverTimezone=UTC";
+    try {
+      koneksi = DriverManager.getConnection(url, "root", "");
+      System.out.println("Berhasil koneksi database");
+    } 
+    catch (SQLException ex) {
+      System.out.println("Gagal koneksi database: " + ex);
+    }
+    
+    return koneksi;
+  }  
+}
